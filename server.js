56d05d030a6c117ea44c96e27420f3d837b81998
Abhishek Ghosh <abhi.ghosh3108@gmail.com>
2023-07-18 12:20:02 +0530

    userlogin added

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const login = require('./routes/login')
 const app = express();
 const port = process.env.PORT;
+app.use(express.json())
 app.use('/',login);
 app.listen(port,()=>{
     console.log(`server is running on ${port}`);
